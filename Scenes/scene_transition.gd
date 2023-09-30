ca482ec3f852b29885d8b2f079eaf515847073d7
amichie1 <34970861+amichie1@users.noreply.github.com>
2023-09-30 19:28:17 -0400

    save scumming

--- Scenes/scene_transition.gd
+++ Scenes/scene_transition.gd
@@ -1,7 +1,16 @@
 extends CanvasLayer
 
 func change_scene(scene: String) -> void:
-	$AnimationPlayer.play('CLOSE')
+	close_door()
 	await $AnimationPlayer.animation_finished
 	get_tree().change_scene_to_file(scene)
+	open_door()
+	
+func close_door():
+	$AnimationPlayer.play('CLOSE')
+	$CloseDoorAudio.play()
+	$DoorClosedTimer.start()
+
+func open_door():
 	$AnimationPlayer.play_backwards('CLOSE')
+	$OpenDoorAudio.play()
