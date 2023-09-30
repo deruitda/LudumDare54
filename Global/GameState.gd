66a95dd90177025a3f067e1fc57f82515457ee5a
deruitda <deruitda@mail.gvsu.edu>
2023-09-30 02:00:58 -0400

    Add closing walls

--- Global/GameState.gd
+++ Global/GameState.gd
@@ -1,6 +1,9 @@
 extends Node
 
 var currentLevel;
+var totalTime = 15.0
+var currentLevelTimeRemaining = totalTime
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	currentLevel = "1-1"
@@ -8,7 +11,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	currentLevelTimeRemaining -= delta
+	print(currentLevelTimeRemaining)
 
 func goto_next_level():
 	pass
