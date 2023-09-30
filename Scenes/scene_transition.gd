e5777f3abbac4a2418b98edbae3950251888f300
amichie1 <34970861+amichie1@users.noreply.github.com>
2023-09-30 19:37:47 -0400

    door transitions

--- Scenes/scene_transition.gd
+++ Scenes/scene_transition.gd
@@ -1,16 +1,27 @@
 extends CanvasLayer
+var has_started = false
 
 func change_scene(scene: String) -> void:
-	close_door()
-	await $AnimationPlayer.animation_finished
-	get_tree().change_scene_to_file(scene)
-	open_door()
+	if has_started == false:
+		has_started = true
+		get_tree().change_scene_to_file(scene)
+		open_door()
+	else:
+		close_door()
+		await $AnimationPlayer.animation_finished
+		get_tree().change_scene_to_file(scene)
+		$DoorClosedTimer.start()
+
 	
 func close_door():
 	$AnimationPlayer.play('CLOSE')
 	$CloseDoorAudio.play()
-	$DoorClosedTimer.start()
 
 func open_door():
 	$AnimationPlayer.play_backwards('CLOSE')
 	$OpenDoorAudio.play()
+
+
+func _on_door_closed_timer_timeout():
+	$DoorClosedTimer.stop()
+	open_door()
