a29c6c80dd7d61cb265734dbffa5727958032a3e
amichie1 <34970861+amichie1@users.noreply.github.com>
2023-10-01 00:28:18 -0400

    wip

--- Scenes/ClosingWalls/closing_walls.gd
+++ Scenes/ClosingWalls/closing_walls.gd
@@ -2,21 +2,22 @@ extends Node2D
 
 @export var delay_in_seconds = 1
 @export var number_of_seconds_until_closed = 15
+@export var direction = Vector2(0, -10)
 var close_speed_for_one_second = 130
 var moving = false
 var level_started = false
-@onready var sound_fall_audio = $SoundFallAudio
+@onready var sand_wall_audio = $SandWallAudio
 
 
 func start_movement():
 	print('start movement')
 	moving = true
-	sound_fall_audio.play()
+	sand_wall_audio.play()
 	
 	
 func stop_movement():
 	moving = false
-	sound_fall_audio.stop()
+	sand_wall_audio.stop()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -26,7 +27,7 @@ func _process(delta):
 		$DelayStartTimer.start()
 	
 	if moving:
-		self.translate(Vector2(0, -10) * delta * (close_speed_for_one_second / number_of_seconds_until_closed))
+		self.translate(direction * delta * (close_speed_for_one_second / number_of_seconds_until_closed))
 
 
 func _on_delay_start_timer_timeout():
