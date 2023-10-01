db3a7efe249b850ea75c4dd8f001304df6abcca1
amichie1 <34970861+amichie1@users.noreply.github.com>
2023-10-01 00:59:06 -0400

    let's do it

--- Scenes/ClosingWalls/closing_walls.gd
+++ Scenes/ClosingWalls/closing_walls.gd
@@ -2,7 +2,19 @@ extends Node2D
 
 @export var delay_in_seconds = 0
 @export var number_of_seconds_until_closed = 15
-@export var direction = Vector2(0, -10)
+enum Direction {
+	UP,
+	UP_RIGHT,
+	RIGHT,
+	DOWN_RIGHT,
+	DOWN,
+	DOWN_LEFT,
+	LEFT,
+	UP_LEFT
+}
+
+@export var direction : Direction = Direction.UP
+
 var close_speed_for_one_second = 130
 var moving = false
 var level_started = false
@@ -33,8 +45,21 @@ func _process(delta):
 			$DelayStartTimer.start()
 	
 	if moving:
-		self.translate(direction * delta * (close_speed_for_one_second / number_of_seconds_until_closed))
+		
+		self.translate(get_vector() * delta * (close_speed_for_one_second / number_of_seconds_until_closed))
 
+func get_vector() -> Vector2:
+	var directionSpeeds = {
+		Direction.UP: Vector2(0, -10),
+		Direction.UP_RIGHT: Vector2(10, -10).normalized(),
+		Direction.RIGHT: Vector2(10, 0),
+		Direction.DOWN_RIGHT: Vector2(10, 10).normalized(),
+		Direction.DOWN: Vector2(0, 10),
+		Direction.DOWN_LEFT: Vector2(-10, 10).normalized(),
+		Direction.LEFT: Vector2(-10, 0),
+		Direction.UP_LEFT: Vector2(-10, -10).normalized()
+	}
+	return directionSpeeds[direction]
 
 func _on_delay_start_timer_timeout():
 	start_movement()
