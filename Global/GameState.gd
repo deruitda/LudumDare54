e7522a1d79a8ba6ef7ce0c1357a7fc82845bd1f5
amichie1 <34970861+amichie1@users.noreply.github.com>
2023-10-01 01:53:47 -0400

    adding long boy level

--- Global/GameState.gd
+++ Global/GameState.gd
@@ -1,8 +1,8 @@
 extends Node
 
-@export var sandbox = false
+@export var sandbox = true
 #@export var sandbox_scene = "res://Scenes/prototype_andrew.tscn"
-@export var sandbox_scene = "res://Scenes/base_level.tscn"
+@export var sandbox_scene = "res://Scenes/Levels/1-2.tscn"
 
 var total_number_of_keys = 0
 var number_of_keys_this_room = 0
