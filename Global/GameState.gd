322aa3e698c54afc1d9c99f01eba141e5b2c45ec
amichie1 <34970861+amichie1@users.noreply.github.com>
2023-10-01 20:04:46 -0400

    dash holding sound

--- Global/GameState.gd
+++ Global/GameState.gd
@@ -1,8 +1,8 @@
 extends Node
 
-@export var sandbox = false
+@export var sandbox = true
 #@export var sandbox_scene = "res://Scenes/prototype_andrew.tscn"
-@export var sandbox_scene = "res://Scenes/Levels/1-4.tscn"
+@export var sandbox_scene = "res://Scenes/Levels/1-5.tscn"
 
 var total_number_of_keys = 0
 var number_of_keys_this_room = 5
