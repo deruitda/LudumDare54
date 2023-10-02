9b51a0f3a2b1df53b437ec29529defaac6d34618
deruitda <deruitda@mail.gvsu.edu>
2023-10-01 23:10:08 -0400

    Add crossbow sprites and arrow death

--- Scenes/arrow_tower.gd
+++ Scenes/arrow_tower.gd
@@ -13,10 +13,12 @@ func _ready():
 func fire():
 	var arrow_scene = preload("res://Scenes/arrow.tscn")
 	var arrow = arrow_scene.instantiate()
+	$Sprite2D.texture = load("res://Assets/image/crossbow/crossbow-fired.png")
 	arrow.direction = direction
 	arrow.speed = speed
 	self.add_child(arrow)
 	$ArrowShotAudio.play()
+	$ReloadTimer.start()
 
 func start_timer():
 	$FireRateTimer.wait_time = (60.0 / fire_rate_per_minute)
@@ -25,3 +27,7 @@ func start_timer():
 func _on_fire_rate_timer_timeout():
 	fire()
 	start_timer()	
+
+
+func _on_reload_timer_timeout():
+	$Sprite2D.texture = load("res://Assets/image/crossbow/crossbow-loaded.png")
