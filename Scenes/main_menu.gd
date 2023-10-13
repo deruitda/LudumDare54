8ba0b745caf492c4b0adf2e0d7a82c6313680dab
amichie1 <34970861+amichie1@users.noreply.github.com>
2023-10-13 11:29:33 -0400

    added font size to go back to main menu button

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -3,7 +3,7 @@ extends Node2D
 #var developer_mode_scene = "res://Scenes/final_scene.gd"
 var developer_mode_scene =  "res://Scenes/Levels/1-6.tscn"
 var developer_mode = false
-var test_transitions = false
+var test_transitions = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
